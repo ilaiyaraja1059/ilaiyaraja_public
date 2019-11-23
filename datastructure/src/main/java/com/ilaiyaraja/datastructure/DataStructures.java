7b4b4b2424511434a24103b57300822696e1df03
ilaiyaraja1059 <ilaiyaraja1059@gmail.com>
2019-11-24 01:03:06 +0530

    Code change added for Queue Data Structure

--- datastructure/src/main/java/com/ilaiyaraja/datastructure/DataStructures.java
+++ datastructure/src/main/java/com/ilaiyaraja/datastructure/DataStructures.java
@@ -4,13 +4,26 @@ public class DataStructures {
 
 	
 	public static void main(String [] arg) {
-		Stack stack = new StackImpl(2);
+		/* 
+		 // Uncomment the below lines to test the stack data structure
+		Stack stack = new StackImpl(5);
 		stack.push(new Student(1,18,"Ilaiyaraja"));
 		stack.push(new Student(2,22,"Ilaiyaraja"));
-		stack.push(new Student(3,30,"Ilaiyaraja"));
-		for(int i=0;i<2;i++) {
 		System.out.println(stack.pop().toString());
-		
-		}
+		stack.push(new Student(3,30,"Ilaiyaraja"));
+		for(int i=0;i<5;i++) {
+		System.out.println(stack.pop().toString());
+		} */
+		Queue queue=new QueueImpl(5);
+		queue.enQueue(new Student(1,18,"Ilaiyaraja"));
+		queue.enQueue(new Student(2,22,"Ilaiyaraja"));
+		queue.enQueue(new Student(3,30,"Ilaiyaraja"));
+		queue.enQueue(new Student(4,30,"Ilaiyaraja"));
+		queue.enQueue(new Student(5,30,"Ilaiyaraja"));
+		System.out.println(queue.deQueue().toString());
+		queue.enQueue(new Student(6,30,"Ilaiyaraja"));
+		for(int i=0;i<7;i++) {
+			System.out.println(queue.deQueue().toString());
+			}
 	}
 }
